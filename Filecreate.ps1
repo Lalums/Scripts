6d7016fe061a8c29e437149603daf276e5032bd2
Lalums <31470639+Lalums@users.noreply.github.com>
2017-08-30 16:44:31 +0530

    Add files via upload

---
+++ Filecreate.ps1
@@ -0,0 +1,13 @@
+﻿configuration createFile
+    {
+    param ($MachineName)
+    Node $MachineName
+       
+        {
+          File CreateFile {
+            Type = 'Directory'
+            DestinationPath = 'C:\Ratheesh'
+            Ensure = "Present"
+            }
+        }
+    }
